c9c03e4609089e4e2a494bbf42af46bffd3e1fba
Madhubabuchamakuri <86195617+Madhubabuchamakuri@users.noreply.github.com>
2021-06-21 11:28:31 +0530

    updated email service

--- src/main/java/com/BasicLogin/util/CommonUtil.java
+++ src/main/java/com/BasicLogin/util/CommonUtil.java
@@ -26,62 +26,28 @@ public class CommonUtil {
         protocol = protocolType;
     }
 
-    public static void sendEmail(String to, String userMessage, String subject, String from) throws IOException, MessagingException {
+      public static void sendEmail(String to, String userMessage, String subject, String from) throws UnknownHostException, MessagingException {
         // Get system properties
-        //Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
         // Get a Properties object
-//         Properties props = System.getProperties();
-//         // Get the default Session object.
-//         Session session = Session.getDefaultInstance(props);
-//         // Create a default MimeMessage object.
-//         MimeMessage message = new MimeMessage(session);
-//         // Set From: header field of the header.
-//         message.setFrom(new InternetAddress(from));
-//         // Set To: header field of the header.
-//         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-//         // Set Subject: header field
-//         message.setSubject(subject);
-//         // Now set the actual message
-//         message.setText(userMessage);
-//         SMTPTransport t = (SMTPTransport) session.getTransport(protocol);
-//         t.connect(host, from, password);
-//         t.sendMessage(message, message.getAllRecipients());
-//         t.close();
+        Properties props = System.getProperties();
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(props);
+        // Create a default MimeMessage object.
+        MimeMessage message = new MimeMessage(session);
+        // Set From: header field of the header.
+        message.setFrom(new InternetAddress(from));
+        // Set To: header field of the header.
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        // Set Subject: header field
+        message.setSubject(subject);
+        // Now set the actual message
+        message.setText(userMessage);
+        SMTPTransport t = (SMTPTransport) session.getTransport(protocol);
+        t.connect(host, from, password);
+        t.sendMessage(message, message.getAllRecipients());
+        t.close();
         // Send message
-        OkHttpClient client = new OkHttpClient();
-
-        MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\n" +
-                "  \"personalizations\": [\n" +
-                "    {\n" +
-                "      \"to\": [\n" +
-                "        {\n" +
-                "          \"email\": "+to+"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"subject\": "+subject+"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"from\": {\n" +
-                "    \"email\": "+from+"\n" +
-                "  },\n" +
-                "  \"content\": [\n" +
-                "    {\n" +
-                "      \"type\": \"text/plain\",\n" +
-                "      \"value\": "+userMessage+"\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}");
-        Request request = new Request.Builder()
-                .url("https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send")
-                .post(body)
-                .addHeader("content-type", "application/json")
-                .addHeader("x-rapidapi-key", "75da3bd872mshea2b4537b8ba2eep10233ajsnedc0bb76d6d8")
-                .addHeader("x-rapidapi-host", "rapidprod-sendgrid-v1.p.rapidapi.com")
-                .build();
-
-        Response response = client.newCall(request).execute();
-        System.out.println("respo"+response.toString());
     }
 
     public static String genearteOTP(int len) {
